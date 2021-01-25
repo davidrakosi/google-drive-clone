3fcaf0ce6d98f7f0ce889c39fde2bdb70e7c3a99
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-25 06:34:10 -0800

    ADD: search input

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,13 +2,22 @@ import React from 'react'
 
 import GDriveLogo from '../../media/google-drive-logo.png'
 
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 const index = () => {
     return (
         <div className='header'>
             <div className="header__logo">
-                <img src={GDriveLogo} alt="Google Drive"/>
+                <img src={GDriveLogo} alt="Google Drive" />
             </div>
-           
+            <div className="header__searchContainer">
+                <div className="header__searchBar">
+                    <SearchIcon />
+                    <input type="text" />
+                    <ExpandMoreIcon />
+                </div>
+            </div>
         </div>
     )
 }
