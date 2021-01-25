d6e7f7b5409fc3e284cded7dd4c671ff8dcd328d
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-25 06:26:44 -0800

    ADD: header logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import GDriveLogo from '../../media/google-drive-logo.png'
+
 const index = () => {
     return (
         <div className='header'>
-            
+            <div className="header__logo">
+                <img src={GDriveLogo} alt="Google Drive"/>
+            </div>
+           
         </div>
     )
 }
