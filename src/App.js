0a49f71b026187b3f05cd47c3f97d8973e850343
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-25 06:22:43 -0800

    CREATE: header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
-      {/* header */}
+      <Header/>
         {/* search bar */}
         {/* icons */}
       {/* sideBar */}
---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div className='header'>
+            
+        </div>
+    )
+}
+
+export default index
