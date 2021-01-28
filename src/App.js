e21d8dd17b19fc9d35d8d80d13b6c34fa9849693
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-28 12:55:25 -0800

    ADD: authentication + firebase integration + dummy data for authentication

--- src/App.js
+++ src/App.js
@@ -2,13 +2,54 @@ import './App.css';
 import Header from './components/header'
 import Sidebar from './components/sidebar'
 
+import GDriveLogo from './media/google-drive-logo.png'
+
+import { auth, provider } from "./firebase";
+import { useState } from 'react';
+
 function App() {
+  const [user, setUser] = useState({
+    displayName: "David Rakosi",
+    email: "david@cleverprogrammer.com",
+    emailVerified: true,
+    phoneNumber: null,
+    photoURL: "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg"
+  })
+
+  const handleLogin = () => {
+    if (!user) {
+      auth.signInWithPopup(provider).then((result) => {
+        setUser(result.user)
+        console.log(result.user)
+      }).catch((error) => {
+        alert(error.message);
+      });
+    } else if (user) {
+      auth.signOut().then(() => {
+        setUser(null)
+      }).catch((err) => alert(err.message))
+    }
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-      {/* files */}
-      {/* icons right */}
+    <div className="app">
+      {
+        user ? (
+          <>
+            <Header />
+            <div className="app__main">
+              <Sidebar />
+              {/* files */}
+              {/* icons right */}
+            </div>
+          </>
+        ) : (
+            <div className='app__login'>
+              <img src={GDriveLogo} alt="Google Drive" />
+              <button onClick={handleLogin}>Log in to Google Drive</button>
+            </div>
+          )
+      }
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyANE39sgac-2bJHy3UQ5UcCLonpJ4OfQ8A",
+    authDomain: "drive-clone-e32b9.firebaseapp.com",
+    projectId: "drive-clone-e32b9",
+    storageBucket: "drive-clone-e32b9.appspot.com",
+    messagingSenderId: "1065309532898",
+    appId: "1:1065309532898:web:58ae8cce733671b27fe4bb"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export {auth, provider}
