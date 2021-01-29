0e4e6d7a20f9d83374aeb34a7564ce05163bdf25
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-01-28 17:21:52 -0800

    ADD: file upload

--- src/firebase.js
+++ src/firebase.js
@@ -13,5 +13,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
+const storage = firebase.storage()
+const db = firebaseApp.firestore()
 
-export {auth, provider}
+export { auth, provider, db, storage }
