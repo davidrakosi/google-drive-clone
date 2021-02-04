2a879abd7323d0b990573da6e7ad4e4c4e622767
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-02-03 19:31:21 -0800

    ADD: titles

--- src/components/filesView/FilesView.js
+++ src/components/filesView/FilesView.js
@@ -30,7 +30,15 @@ const FilesView = () => {
 
                 }
             </div>
-
+            <div className="fileView__titles">
+                <div className="fileView__titles--left">
+                    <p>Name</p>
+                </div>
+                <div className="fileView__titles--right">
+                    <p>Last modified</p>
+                    <p>File size</p>
+                </div>
+            </div>
             {
                 files.map(({ id, item }) => (
                     <FileItem id={id} caption={item.caption} timestamp={item.timestamp} fileUrl={item.fileUrl} size={item.size} />
