d3175bc223b855ea6d8a538307e6760bc2e7594c
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-02-04 16:38:05 -0800

    CHNG: fake data

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import { auth, provider } from "./firebase";
 import { useState } from 'react';
 
 function App() {
-  // const [user, setUser] = useState()
-  const [user, setUser] = useState({
-    displayName: "David Rakosi",
-    email: "david@cleverprogrammer.com",
-    emailVerified: true,
-    phoneNumber: null,
-    photoURL: "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg"
-  })
+  const [user, setUser] = useState()
+  // const [user, setUser] = useState({
+  //   displayName: "David Rakosi",
+  //   email: "david@cleverprogrammer.com",
+  //   emailVerified: true,
+  //   phoneNumber: null,
+  //   photoURL: "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg"
+  // })
 
   const handleLogin = () => {
     if (!user) {
